4227498975cfde5b222109e98b959868a375900b
erpascua <erpascua@student.42.fr>
2026-05-03 15:40:41 +0200

    (fix): mariadb: script not working

--- srcs/requirements/mariadb/tools/mariadb.sh
+++ srcs/requirements/mariadb/tools/mariadb.sh
@@ -1,19 +1,9 @@
-# **************************************************************************** #
-#                                                                              #
-#                                                         :::      ::::::::    #
-#    mariadb.sh                                         :+:      :+:    :+:    #
-#                                                     +:+ +:+         +:+      #
-#    By: erpascua <erpascua@student.42.fr>          +#+  +:+       +#+         #
-#                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2026/04/30 16:16:01 by erpascua          #+#    #+#              #
-#    Updated: 2026/04/30 16:16:04 by erpascua         ###   ########.fr        #
-#                                                                              #
-# **************************************************************************** #
-
 #!/bin/sh
 
-FLAG="/var/lib/mysql/.flag"
-DB_DIR="/var/lib/mysql/wordpress"
+set -e
+
+FLAG="/var/lib/mysql/.initialized"
+DB_DIR="/var/lib/mysql/${MYSQL_DATABASE}"
 
 rm -f /var/run/mysqld/mysqld.pid
 rm -f /var/run/mysqld/mysqld.sock
@@ -21,14 +11,13 @@ mkdir -p /var/run/mysqld
 chown -R mysql:mysql /var/run/mysqld
 chown -R mysql:mysql /var/lib/mysql
 
-if [ ! -f "$FLAG" ] || [ ! -d "$DB_DIR" ] ; then
-
-    echo "Init MariaDB"
+if [ ! -f "$FLAG" ] || [ ! -d "$DB_DIR" ]; then
+    echo "Initializing MariaDB"
 
     service mariadb start
 
-    until mysqladmin ping >/dev/null 2>&1; do
-        echo "Waiting MariaDB"
+    until mysqladmin ping --silent >/dev/null 2>&1; do
+        echo "Waiting for MariaDB"
         sleep 2
     done
 
@@ -36,12 +25,13 @@ if [ ! -f "$FLAG" ] || [ ! -d "$DB_DIR" ] ; then
     mysql -u root -e "CREATE USER IF NOT EXISTS \`${MYSQL_USER}\`@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
     mysql -u root -e "GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO \`${MYSQL_USER}\`@'%';"
     mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
-    mysql -u root -p${MYSQL_ROOT_PASSWORD} -e "FLUSH PRIVILEGES;"
-    mysqladmin -u root -p${MYSQL_ROOT_PASSWORD} shutdown
+    mysql -u root -p"${MYSQL_ROOT_PASSWORD}" -e "FLUSH PRIVILEGES;"
+
+    mysqladmin -u root -p"${MYSQL_ROOT_PASSWORD}" shutdown
     touch "$FLAG"
-    echo "MariaDB installed"
+    echo "MariaDB initialized"
 else
-    echo "MariaDB already installed"
+    echo "MariaDB already initialized"
 fi
 
 exec mysqld_safe --bind-address=0.0.0.0
